17060f351043a56c5116e025825af09947f528e0
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2023-11-29 12:44:45 +0100

    answer button ui

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,6 +11,38 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Questions Screen"));
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "The question......",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(
+            answerText: "Answer 1",
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: "Answer 2",
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: "Answer 3",
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: "Answer 4",
+            onTap: () {},
+          ),          
+        ],
+      ),
+    );
   }
 }
