56a4126a5a72719df8385afb05a88bb36112ac00
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2023-11-29 15:58:13 +0100

    dynamically inserting number of correct question

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -24,6 +24,12 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -32,11 +38,12 @@ class ResultsScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const Text("You answered x out y question correctly"),
+            Text(
+                "You answered $numCorrectQuestions out of $numTotalQuestions question correctly"),
             const SizedBox(
               height: 30,
             ),
-            QuestionsSummary(summaryData: getSummaryData()),
+            QuestionsSummary(summaryData: summaryData),
             const SizedBox(
               height: 30,
             ),
