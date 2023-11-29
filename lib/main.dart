50705ee843fffd0a1df5f09834698d0126d6a89a
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2023-11-29 11:59:40 +0100

    Button functionality to switch from start screen to questions screen(state lifting up

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/gradient_container.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home:
-            // GradientContainer(
-            //   Color.fromARGB(255, 118, 58, 223),
-            //   Color.fromARGB(255, 79, 25, 173),
-            // ),
-            Scaffold(
-          body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 78, 13, 151),
-                  Color.fromARGB(255, 107, 15, 168),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
-            child: const StartScreen(),
-          ),
-        )),
+    const Quiz(),
   );
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  // Widget? activeScreen;
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   activeScreen = StartScreen(switchScreen);
+  // }
+
+  // void switchScreen() {
+  //   setState(() {
+  //     activeScreen = const QuestionsScreen();
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:
+          // GradientContainer(
+          //   Color.fromARGB(255, 118, 58, 223),
+          //   Color.fromARGB(255, 79, 25, 173),
+          // ),
+          Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          // child: activeScreen,
+          // child: activeScreen == 'start-screen'
+          //     ? StartScreen(switchScreen)
+          //     : const QuestionsScreen(),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
