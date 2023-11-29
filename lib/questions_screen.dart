718a873e209f7fca40c8a062598c019b335a07bc
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2023-11-29 14:40:38 +0100

    results screen ui and showing result screen after answering questions

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -28,10 +28,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
   Widget build(BuildContext context) {
     final currentQuestion = questions[currentQuestionIndex];
 
-    return Container(
-      margin: const EdgeInsets.all(40),
-      child: SizedBox(
-        width: double.infinity,
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
