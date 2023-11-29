be25428633369a3283fbfe8f06e99f5443a7135a
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2023-11-29 01:10:59 +0100

    start screen ui complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/gradient_container.dart';
+import 'package:quiz_app/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: GradientContainer(
-        Color.fromARGB(255, 118, 58, 223),
-        Color.fromARGB(255, 79, 25, 173),
-      ),
-    ),
+    MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home:
+            // GradientContainer(
+            //   Color.fromARGB(255, 118, 58, 223),
+            //   Color.fromARGB(255, 79, 25, 173),
+            // ),
+            Scaffold(
+          body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 78, 13, 151),
+                  Color.fromARGB(255, 107, 15, 168),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: const StartScreen(),
+          ),
+        )),
   );
 }
